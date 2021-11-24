3eff431a9aa8402a729eb8ec1cdfbe111126909f
kazuki13 <kazuki.mk.13@gmail.com>
2021-11-24 14:26:21 +0900

    test2

---
+++ src/components/uplode.vue
@@ -0,0 +1,26 @@
+
+<template>
+  <div class="app-container">
+    <div id="lightgallery">
+      <a href=".\assets\logo.png">
+        <img src=".\assets\logo.png" width="100" height="100" />
+        
+      </a>
+    </div>
+  </div>
+</template>
+import 'lightgallery.js'
+import 'lightgallery.js/dist/css/lightgallery.css'
+
+export default {
+  name: 'IssueDetail',
+  data() {
+    return {
+      imageList: []
+    }
+  },
+  mounted() {
+    const el = document.getElementById('lightgallery')
+    window.lightGallery(el)
+  }
+}
