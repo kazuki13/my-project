b2d8740a7f08a2264132332801f4db034dad48fa
kazuki13 <kazuki.mk.13@gmail.com>
2022-02-05 22:27:57 +0900

    aaaaaa

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,100 +1,69 @@
 
 
-<template id="main">
-  <div id="app">
-    <canvas ref="canvas" id="emo_canvas" width="400" height="400"></canvas>
-    <div>
-      <video ref="video" id="video" width="400" height="400" playsinline muted autoplay></video>
-    </div>
+<script>
+const { AnonymousCredential, BlobServiceClient, newPipeline } = require('@azure/storage-blob');
+// const { v1: uuidv1} = require('uuid');
+const fs = require("fs");
+async function main() {
+    console.log('Azure Blob storage v12 - JavaScript quickstart sample');
+    // Quick start code goes here
+    // Retrieve the connection string for use with the application. The storage
+    // connection string is stored in an environment variable on the machine
+    // running the application called AZURE_STORAGE_CONNECTION_STRING. If the
+    // environment variable is created after the application is launched in a
+    // console or with Visual Studio, the shell or application needs to be closed
+    // and reloaded to take the environment variable into account.
+  // Fill in following settings before running this sample
+  const account = process.env.ACCOUNT_NAME || "pass";
+  const accountSas = process.env.ACCOUNT_SAS || "";
+  const localFilePath = "face.png";
+
+  const pipeline = newPipeline(new AnonymousCredential(), {
+    // httpClient: MyHTTPClient, // A customized HTTP client implementing IHttpClient interface
+    retryOptions: { maxTries: 4 }, // Retry options
+    userAgentOptions: { userAgentPrefix: "AdvancedSample V1.0.0" }, // Customized telemetry string
+    keepAliveOptions: {
+      // Keep alive is enabled by default, disable keep alive by setting false
+      enable: false
+    }
+    });
+    const blobServiceClient = new BlobServiceClient(
+        `https://${account}.blob.core.windows.net${accountSas}`,
+        pipeline
+    );
+    console.log("bjkl;g")
+    // Create a unique name for the container
+    const containerName = `image`;
+    console.log('\nCreating container...');
+    console.log('\t', containerName);
+
+    // Get a reference to a container
+    
+    const containerClient = blobServiceClient.getContainerClient(containerName);
+    console.log('\nListing blobs...');
+
+	console.log('\nUploading to Azure storage as blob:\n\t', containerName);
+
+	// Upload data to the blob
+  try {
+    await containerClient.uploadStream(fs.createReadStream(localFilePath), 4 * 1024 * 1024, 20, {
+      abortSignal: AbortController.timeout(30 * 60 * 1000), // Abort uploading with timeout in 30mins
+      onProgress: (ev) => console.log(ev)
+    });
+    console.log("uploadStream succeeds");
+  } catch (err) {
+    console.log(
+      `uploadStream failed, requestId - ${err.details.requestId}, statusCode - ${err.statusCode}, errorCode - ${err.details.errorCode}`
+    );
+  }
+
+	// console.log("Blob was uploaded successfully. requestId: ", uploadBlobResponse.requestId);
+}
+
+main().then(() => console.log('Done')).catch((ex) => console.log(ex.message));
+</script>
+<template>
+  <div>
+    AZURE_STORAGE_ACCOUNT_ACCESS_KEY
   </div>
 </template>
-
-<script>
-  import Vue from 'vue';
-  import Axios from 'axios';
-  import VueAxios from 'vue-axios'
-
-  Vue.use(VueAxios, Axios)
-
-  export default ({
-    template: '#main',
-    data() {
-      return {
-        video: {},
-        canvas: {},
-        captures: [],
-        testTimer: '',
-      };
-    },
-    mounted() {
-    //カメラを起動し、映像をvideoタグ内に表示
-      this.video = this.$refs.video
-      if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-        navigator.mediaDevices.getUserMedia({audio: false, video: true})
-        .then(stream => {
-          this.video.srcObject = stream
-          this.video.play()
-        })
-      }
-
-      console.log(this.$refs.canvas)
-      // console.log(this.$refs.canvas)
-
-      this.canvas = this.$refs.canvas 
-      this.testTimer = setInterval(() => {
-        // console.log(this.$refs.canvas)
-        let context = this.canvas.getContext("2d")
-        context.drawImage(this.video, 0, 0, 400, 240)
-        this.captures.push(this.canvas.toDataURL("image/png")) //撮った画像をcaptures配列に格納する
-        let subscriptionKey = "";
-        let uriBase = ".com/face/v1.0/detect";
-        // let params = {
-        //   "returnFaceId": "true",
-        //   "returnFaceLandmarks": "false",
-        //   "returnFaceAttributes":
-        //     "emotion"
-        // };
-        //配列最後に追加された画像のフォーマットを変換し、imgURL変数に代入する
-        const imgURL = this.makeblob(this.captures[this.captures.length - 1])
-        //imgURLの画像をFaceAPIに送信
-        Axios.post(
-          uriBase + "?returnFaceId=true&returnFaceLandmarks=false&returnFaceAttributes=age,emotion",
-          imgURL,
-          {
-            headers: {
-              "Content-Type": "application/octet-stream",
-              "Ocp-Apim-Subscription-Key": subscriptionKey,
-            }
-          },
-        )
-        .then(response => {
-          console.log(response.data[0].faceAttributes.emotion)
-
-        })
-        .catch(error => {
-          console.log(error.response)
-        });
-      }, 8000);
-    },
-    methods: {     
-      makeblob: function (dataURL) {
-        let BASE64_MARKER = ';base64,';
-        if (dataURL.indexOf(BASE64_MARKER) == -1) {
-          let parts = dataURL.split(',');
-          let contentType = parts[0].split(':')[1];
-          let raw = decodeURIComponent(parts[1]);
-          return new Blob([raw], {type: contentType});
-        }
-        let parts = dataURL.split(BASE64_MARKER);
-        let contentType = parts[0].split(':')[1];
-        let raw = window.atob(parts[1]);
-        let rawLength = raw.length;
-        let uInt8Array = new Uint8Array(rawLength);
-        for (let i = 0; i < rawLength; ++i) {
-          uInt8Array[i] = raw.charCodeAt(i);
-        }
-        return new Blob([uInt8Array], {type: contentType})
-      }
-    }
-  });
-</script>  
